a054e0839abbeecb8eabbe7715b003bc078ff643
EUNYOUNGYOON <milkoon1@naver.com>
2020-08-05 20:37:48 +0900

    ClickMeButton

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200805/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200805/MainActivity.kt
@@ -2,10 +2,22 @@ package kr.co.tjoeun.kotlinbasic_20200805
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Ctrl + / : 해당 줄을 주석, 메모처럼 주석을 적자
+        // xml 에 그려둔 Click Me 버튼이 눌렸다는걸 감지
+        clickMeBtn.setOnClickListener {
+
+            // 이 중괄호 코드가 clickMe 버튼이 눌리면 실행
+            Log.d("버튼클릭", "ClickMeButton Push!")
+
+        }
+
     }
 }
